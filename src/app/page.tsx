366143db0293acab0734c7236bb383d5c84d4eec
Ricardo Casares <ricardocasares@gmail.com>
2024-10-06 14:05:37 +0200

    fix: colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import {Other} from '@/icons/other'
 
 export default async function Page() {
   return (
-    <div className='h-screen bg-base-200 flex flex-col items-center justify-center gap-2'>
+    <div className='h-screen flex flex-col items-center justify-center gap-2'>
       <Other className='w-24 h-24 text-success' />
       <h1 className='text-neutral-content'>
         You don&apos;t have any saved stops
