f17139ea6e7e2307d20110dbe553ba13f1935fb6
Ricardo Casares <ricardocasares@gmail.com>
2024-10-06 14:02:00 +0200

    fix: escape char

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default async function Page() {
   return (
     <div className='h-screen bg-base-200 flex flex-col items-center justify-center gap-2'>
       <Other className='w-24 h-24 text-success' />
-      <h1 className='text-neutral-content'>You don't have any saved stops</h1>
+      <h1 className='text-neutral-content'>
+        You don&apos;t have any saved stops
+      </h1>
     </div>
   )
 }
