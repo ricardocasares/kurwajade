bfa681a1cdb59f43fe4bc81a0f5f34fa3efdc98b
Ricardo Casares <ricardocasares@gmail.com>
2024-10-06 14:13:55 +0200

    feat: update tram stop info with dynamic API data and improved bus icon

--- src/icons/bus.tsx
+++ src/icons/bus.tsx
@@ -14,9 +14,10 @@ export function Bus(props: SVGProps<SVGSVGElement>) {
         strokeLinecap='round'
         strokeLinejoin='round'
         strokeWidth='2'>
-        <path d='M4 6L2 7m8-1h4m8 1l-2-1'></path>
-        <rect width='16' height='16' x='4' y='3' rx='2'></rect>
-        <path d='M4 11h16M8 15h.01M16 15h.01M6 19v2m12 0v-2'></path>
+        <path d='M8 6v6m7-6v6M2 12h19.6M18 18h3s.5-1.7.8-2.8c.1-.4.2-.8.2-1.2s-.1-.8-.2-1.2l-1.4-5C20.1 6.8 19.1 6 18 6H4a2 2 0 0 0-2 2v10h3'></path>
+        <circle cx='7' cy='18' r='2'></circle>
+        <path d='M9 18h5'></path>
+        <circle cx='16' cy='18' r='2'></circle>
       </g>
     </svg>
   )
